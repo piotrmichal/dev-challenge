2263f992ac481bde3c307d43e6ab76d3d4e34ffc
Piotr Michalczuk <piotrekmichal91@gmail.com>
2016-11-24 14:05:39 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+$(function() {
+	$('.fancybox').fancybox({
+		helpers: {
+			overlay: {
+				css: {
+					'background-color': 'rgba(0, 124, 243, .7)'
+				}
+			}
+		}
+	});
+
+	$('.menu-icon').on('click', function() {
+		$('header nav ul').toggleClass('open');
+	})
+
+	var $window = $(window);
+	var $nav = $('nav');
+	var $listItems = $('li');
+	var $link = $('li a');
+	var navHeight = $nav.outerHeight();
+
+	$link.on('click', function(e) {
+		e.preventDefault();
+		var target = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top
+		}, 500, 'swing');
+		$listItems.removeClass('active');
+		$(this).parent().addClass('active');
+	});
+
+	$window.on('scroll', function() {
+		var $scrollPos = $window.scrollTop();
+		if($scrollPos > 0){
+			$nav.addClass('stickyStyle');
+		} else if ($scrollPos <= 10) {
+			$nav.removeClass('stickyStyle');
+		}
+
+		var $section = $('section');
+		$section.each(function() {
+			var $this = $(this);
+			if($this.offset().top < ($scrollPos + navHeight) && 
+				($scrollPos + navHeight) < ($this.offset().top + $this.outerHeight())) {
+				var targetClass = '.' + $this.attr('id');
+				$listItems.removeClass('active');
+				$(targetClass).addClass('active');
+				if($this.attr('id') === 'client') {
+					$listItems.removeClass('active');
+					$('li.pages').addClass('active');
+				}
+			} else if($scrollPos < (800 - navHeight)) {
+				$listItems.removeClass('active');
+				$('li.home').addClass('active');
+			}
+		});
+	});
+});
